2f9309f4805f384ee0549edfbb447ea815e917c1
Naif Aldawsari <NaifDW@outlook.sa>
2023-12-11 18:49:48 +0300

    make menu better

--- src/javalibrarysystem/MainUI.java
+++ src/javalibrarysystem/MainUI.java
@@ -7,6 +7,7 @@ import javalibrarysystem.panels.*;
 import java.awt.*;
 
 public class MainUI extends JFrame {
+
     private JPanel cardPanel;
     private CardLayout cardLayout;
 
@@ -27,7 +28,6 @@ public class MainUI extends JFrame {
         // cardPanel.add(new BookPanel(this), "BookPanel");
         // cardPanel.add(new UserPanel(this), "UserPanel");
         // ...
-
         // Initialize the menu and other UI components
         initUI();
 
@@ -47,35 +47,47 @@ public class MainUI extends JFrame {
         // Create and add JMenuBar with menu items
         // You can use menu items to switch between cards, for example:
         JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("Navigate");
+
+        JMenu navigateMenu = new JMenu("Navigate");
         JMenuItem loginItem = new JMenuItem("Login");
         loginItem.addActionListener(e -> switchPanel("LoginPanel"));
         JMenuItem registerItem = new JMenuItem("Register");
         registerItem.addActionListener(e -> switchPanel("RegistrationPanel"));
+        navigateMenu.add(loginItem);
+        navigateMenu.add(registerItem);
 
+        JMenu usersMenu = new JMenu("Users");
         JMenuItem usersItem = new JMenuItem("Users");
         usersItem.addActionListener(e -> switchPanel("UsersPanel"));
+        usersMenu.add(usersItem);
 
+        JMenu booksMenu = new JMenu("Books");
         JMenuItem booksItem = new JMenuItem("Books");
         booksItem.addActionListener(e -> switchPanel("BooksPanel"));
+        booksMenu.add(booksItem);
 
+        JMenu genresMenu = new JMenu("Genres");
         JMenuItem genresItem = new JMenuItem("Genres");
         genresItem.addActionListener(e -> switchPanel("GenresPanel"));
+        genresMenu.add(genresItem);
 
+        JMenu authorsMenu = new JMenu("Authors");
         JMenuItem authorsItem = new JMenuItem("Authors");
         authorsItem.addActionListener(e -> switchPanel("AuthorsPanel"));
+        authorsMenu.add(authorsItem);
 
+        JMenu borrowedBooksMenu = new JMenu("Borrowed Books");
         JMenuItem bookAuthorsItem = new JMenuItem("Borrowed Books");
         bookAuthorsItem.addActionListener(e -> switchPanel("BorrowedBooksPanel"));
-        // Add menu items for other panels
-        menu.add(loginItem);
-        menu.add(registerItem);
-        menu.add(usersItem);
-        menu.add(booksItem);
-        menu.add(genresItem);
-        menu.add(authorsItem);
-        menu.add(bookAuthorsItem);
-        menuBar.add(menu);
+        borrowedBooksMenu.add(bookAuthorsItem);
+
+        menuBar.add(navigateMenu);
+        menuBar.add(usersMenu);
+        menuBar.add(booksMenu);
+        menuBar.add(genresMenu);
+        menuBar.add(authorsMenu);
+        menuBar.add(borrowedBooksMenu);
+
         setJMenuBar(menuBar);
     }
 
